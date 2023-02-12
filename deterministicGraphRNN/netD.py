a45481aa038107188cdb33f18aee0c955512c876
jgeng <jgeng99>
2023-02-11 18:03:17 -0800

    finish debugging netD.py

--- deterministicGraphRNN/netD.py
+++ deterministicGraphRNN/netD.py
@@ -66,13 +66,14 @@ class netD(nn.Module):
             np.array(nx.degree_histogram(G)),
             bins=self.stat_input_dim, range=(0.0, 1.0), density=False)
         # print(type(degree_hist))
-        degree_hist = torch.from_numpy(degree_hist)
+        degree_hist = torch.from_numpy(degree_hist).type(torch.FloatTensor)
         degree_hist = self.stat_NNs[0](degree_hist)
 
         clustering_coefs, _ = np.histogram(
             list(nx.clustering(G).values()),
             bins=self.stat_input_dim, range=(0.0, 1.0), density=False)
-        clustering_coefs = self.statNNs[1](clustering_coefs)
+        clustering_coefs = torch.from_numpy(clustering_coefs).type(torch.FloatTensor)
+        clustering_coefs = self.stat_NNs[1](clustering_coefs)
 
         # stat 1
         stats = torch.Tensor([degree_hist, clustering_coefs])
