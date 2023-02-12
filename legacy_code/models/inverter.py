e105853b0a02822ba1a098bfd903339d1912542c
winston-yu <winston.yu45@gmail.com>
2023-02-11 17:38:02 -0800

    fixed inverter's loss function to include GW distance

--- legacy_code/models/inverter.py
+++ legacy_code/models/inverter.py
@@ -7,6 +7,11 @@ from karateclub import Graph2Vec
 from torch_geometric.datasets import TUDataset
 from torch_geometric.utils import to_networkx
 
+import numpy as np
+import networkx as nx
+import ot # Python Optimal Transport
+from ot.gromov import gromov_wasserstein
+
 class Inverter(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim):
         """
@@ -55,7 +60,6 @@ class WGAN_ReconLoss(nn.Module):
         else:
             print("Loss function must be 'MSE' or 'CrossEntropy'.")
 
-
     def forward(self, x_original, x_reconst, z_original, z_reconst, use_gw1=True):
         """
         From the Generating Natural Adv. Example Paper:
@@ -65,9 +69,20 @@ class WGAN_ReconLoss(nn.Module):
             z_reconst: reconstruction of the noise, output of the inverter
         """
         if use_gw1:
-            return (self.L(x_original, x_reconst) + self.lamb * self.L(z_original, z_reconst)).to(self.device)
-        else:
-            return (wasserstein_distance(x_original, x_reconst) + self.lamb * self.L(z_original, z_reconst)).to(self.device)
+            return (self.L(x_original, x_reconst) \
+                + self.lamb * self.L(z_original, z_reconst)).to(self.device)
+        else:            
+            # initialize measures for each metric measure network
+            p_o = ot.unif(x_original.number_of_nodes())
+            p_r = ot.unif(x_reconst.number_of_nodes())
+            # get adjacency matrices
+            C_o = np.asarray(nx.adjacency_matrix(x_original).todense())
+            C_r = np.asarray(nx.adjacency_matrix(x_reconst).todense())
+            _, log = gromov_wasserstein(
+                C_o, C_r, p_o, p_r, 
+                'square_loss', verbose=False, log=False)
+
+            return (log['gw_dist'] + (self.lamb * self.L(z_original, z_reconst)).to(self.device))
 
 def train(graphs_name, noise_dim, inverter, generator, epochs=10, lr=0.01, batch_size=1, lamb=0.1, loss_func='MSE', device= choose_device()):
     # initialize noise, optimizer and loss
